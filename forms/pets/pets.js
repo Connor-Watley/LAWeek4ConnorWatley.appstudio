e93f78d21ec2185759d50777230a937966b84a9e
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-02-23 18:05:06 -0600

    Create pets.js

---
+++ forms/pets/pets.js
@@ -0,0 +1,4 @@
+let petNames = ["Darcy", "Jerry","Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam"]
+let withSal = petNames.push("Sal")
+console.log(`The third pet is named ${petNames[2]}`)
+console.log(`The array has ${petNames.length}`)
