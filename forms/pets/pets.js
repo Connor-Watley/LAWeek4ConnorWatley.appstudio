2ec4eefb8faf53aa2657957835af19416f16a417
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-02-24 15:42:20 -0600

    Update pets.js

--- forms/pets/pets.js
+++ forms/pets/pets.js
@@ -2,3 +2,5 @@ let petNames = ["Darcy", "Jerry","Marty", "Vito", "Vinny", "Riley", "Saddie", "M
 let withSal = petNames.push("Sal")
 console.log(`The third pet is named ${petNames[2]}`)
 console.log(`The array has ${petNames.length}`)
+petNames.pop();
+
